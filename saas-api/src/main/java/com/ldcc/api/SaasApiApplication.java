f058ab1aca1836524067ec5ab7a41b40f6ab50e6
Jankin-Wu <2938567281@qq.com>
2021-08-04 16:55:27 +0800

    菜单增删改查

---
+++ saas-api/src/main/java/com/ldcc/api/SaasApiApplication.java
@@ -0,0 +1,18 @@
+package com.ldcc.api;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//@EnableSwagger2
+@MapperScan(basePackages = "com.ldcc.**.mapper")// 使用MapperScan批量扫描所有的Mapper接口；
+@SpringBootApplication
+public class SaasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasApiApplication.class, args);
+    }
+
+}
